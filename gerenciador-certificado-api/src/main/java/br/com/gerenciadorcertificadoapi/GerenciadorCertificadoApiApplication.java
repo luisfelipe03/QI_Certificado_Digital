8a012e95a597ef776ec4ea4d4d2fb6de687e1d08
Luis Felipe <llipeandrade34@gmail.com>
2024-04-27 20:27:59 -0300

    troca de projeto

---
+++ gerenciador-certificado-api/src/main/java/br/com/gerenciadorcertificadoapi/GerenciadorCertificadoApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.gerenciadorcertificadoapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GerenciadorCertificadoApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GerenciadorCertificadoApiApplication.class, args);
+    }
+
+}
---
+++ gerenciador-certificado-api/src/test/java/br/com/gerenciadorcertificadoapi/GerenciadorCertificadoApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.gerenciadorcertificadoapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GerenciadorCertificadoApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
