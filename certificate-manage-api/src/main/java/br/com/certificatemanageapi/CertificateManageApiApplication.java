9e7b18f594c355d533ad0f11247cb0b10aa47ecc
Luis Felipe <llipeandrade34@gmail.com>
2024-04-27 10:43:09 -0300

    add init project

---
+++ certificate-manage-api/src/main/java/br/com/certificatemanageapi/CertificateManageApiApplication.java
@@ -0,0 +1,13 @@
+package br.com.certificatemanageapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CertificateManageApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CertificateManageApiApplication.class, args);
+	}
+
+}
---
+++ certificate-manage-api/src/test/java/br/com/certificatemanageapi/CertificateManageApiApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.certificatemanageapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CertificateManageApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
