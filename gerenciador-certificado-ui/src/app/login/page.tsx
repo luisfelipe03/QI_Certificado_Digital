40d0846756fcc9e2c90f2088a6a73dacb6859640
Luis Felipe <llipeandrade34@gmail.com>
2024-05-28 11:29:47 -0300

    add

--- gerenciador-certificado-ui/src/app/login/page.tsx
+++ gerenciador-certificado-ui/src/app/login/page.tsx
@@ -5,6 +5,8 @@ import { useFormik } from 'formik';
 import { CadastroForm, ValidationCadastroScheme, formCadastroScheme } from './CadastroSchema';
 import { LoginForm, ValidationLoginScheme, formLoginScheme } from './LoginSchema';
 import { Button, FieldError, InputText, RenderIf, Template } from '@/components';
+import { FaEnvelope, FaLock, FaUser } from 'react-icons/fa';
+import 'tailwindcss/tailwind.css';
 
 export default function LoginPage() {
     const [loading, setLoading] = useState<boolean>(false);
@@ -31,116 +33,138 @@ export default function LoginPage() {
     });
     
     return (
-        <div>
-            <Template loading={loading}>
-                <div className='flex min-h-full flex-1 flex-col justify-center px-6 py-2 lg:px-8'>
-                    
-                
-                    <div className='sm:mx-auto sm:w-full sm:max-w-sm'>
-                        <h2 className='text-center text-1xl font-bold leading-9 tracking-tight text-gray-900'>
-                            {
-                                newUserState ? 'Cadastro' : 'Login'
-                            }
+        <div className="min-h-screen flex items-center justify-center bg-gray-100">
+                <div className="flex min-h-full flex-1 flex-col justify-center px-6 py-2 lg:px-8">
+                    <div className="sm:mx-auto sm:w-full sm:max-w-md text-center">
+                        <img src="" alt="Logo" className="mx-auto h-12 w-auto" />
+                        <h2 className="mt-6 text-center text-3xl font-extrabold text-gray-900">
+                            {newUserState ? 'Cadastro' : 'Login'}
                         </h2>
                     </div>
 
-                    <div className='mt-4 sm:mx-auto sm:w-full sm:max-w-sm'>
-                        <RenderIf condition={!newUserState}>
-                            <form onSubmit={loginFormik.handleSubmit} className='space-y-2'>
-                                <div>
-                                    <label className='block text-s font-medium leading-6 text-gray-900'>Email: *</label>
-                                </div>
-                                <div className='mt-2'>
-                                    <InputText style='w-full' 
-                                                id='email' 
-                                                placeholder='Email'
+                    <div className="mt-8 sm:mx-auto sm:w-full sm:max-w-md">
+                        <div className="bg-white py-8 px-6 shadow rounded-lg sm:px-10">
+                            <RenderIf condition={!newUserState}>
+                                <form onSubmit={loginFormik.handleSubmit} className="space-y-6">
+                                    <div>
+                                        <label className="block text-sm font-medium text-gray-700">Email</label>
+                                        <div className="mt-1 relative rounded-md shadow-sm">
+                                            <div className="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none">
+                                                <FaEnvelope className="text-gray-400" />
+                                            </div>
+                                            <InputText
+                                                style="w-full pl-10 pr-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+                                                id="email"
+                                                placeholder="Email"
                                                 value={loginFormik.values.email}
-                                                onChange={loginFormik.handleChange} />
-                                    <FieldError error={loginFormik.errors.email} />
-                                </div>
-                                <div>
-                                    <label className='block text-s font-medium leading-6 text-gray-900'>Senha: *</label>
-                                </div>
-                                <div className='mt-2'>
-                                    <InputText style='w-full' 
-                                                id='password' 
-                                                placeholder='Senha'
-                                                type='password'
+                                                onChange={loginFormik.handleChange}
+                                            />
+                                        </div>
+                                        <FieldError error={loginFormik.errors.email} />
+                                    </div>
+
+                                    <div>
+                                        <label className="block text-sm font-medium text-gray-700">Senha</label>
+                                        <div className="mt-1 relative rounded-md shadow-sm">
+                                            <div className="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none">
+                                                <FaLock className="text-gray-400" />
+                                            </div>
+                                            <InputText
+                                                style="w-full pl-10 pr-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+                                                id="password"
+                                                placeholder="Senha"
+                                                type="password"
                                                 value={loginFormik.values.password}
-                                                onChange={loginFormik.handleChange} />
-                                    <FieldError error={loginFormik.errors.password} />
-                                </div>
-                                <Button type='submit' 
-                                                style='bg-indigo-500 hover:bg-indigo-700' 
-                                                label='Entrar'/>
+                                                onChange={loginFormik.handleChange}
+                                            />
+                                        </div>
+                                        <FieldError error={loginFormik.errors.password} />
+                                    </div>
 
-                                <Button type='button' 
-                                        onClick={event => setNewUserState(true)} 
-                                        style='mb-10 ml-2 bg-green-600 hover:bg-green-500' 
-                                        label='Cadastrar'/>
-                            </form>
-                        </RenderIf>
-                        <RenderIf condition={newUserState}>
-                            <form onSubmit={cadastroFormik.handleSubmit} className='space-y-2'>
-                                <div>
-                                    <label className='block text-s font-medium leading-6 text-gray-900'>Nome: *</label>
-                                </div>
-                                <div className='mt-2'>
-                                    <InputText style='w-full' 
-                                                id='name' 
-                                                placeholder='Nome'
+                                    <Button type="submit" style="w-full bg-indigo-500 hover:bg-indigo-700 text-white py-2 px-4 rounded-md" label="Entrar" />
+                                    <Button type="button" onClick={() => setNewUserState(true)} style="w-full bg-green-600 hover:bg-green-500 text-white py-2 px-4 rounded-md mt-4" label="Cadastrar" />
+                                </form>
+                            </RenderIf>
+
+                            <RenderIf condition={newUserState}>
+                                <form onSubmit={cadastroFormik.handleSubmit} className="space-y-6">
+                                    <div>
+                                        <label className="block text-sm font-medium text-gray-700">Nome</label>
+                                        <div className="mt-1 relative rounded-md shadow-sm">
+                                            <div className="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none">
+                                                <FaUser className="text-gray-400" />
+                                            </div>
+                                            <InputText
+                                                style="w-full pl-10 pr-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+                                                id="name"
+                                                placeholder="Nome"
                                                 value={cadastroFormik.values.name}
-                                                onChange={cadastroFormik.handleChange} />
-                                    <FieldError error={cadastroFormik.errors.name} />
-                                </div>
-                                <div>
-                                    <label className='block text-s font-medium leading-6 text-gray-900'>Email: *</label>
-                                </div>
-                                <div className='mt-2'>
-                                    <InputText style='w-full' 
-                                                id='email' 
-                                                placeholder='Email'
-                                                value={cadastroFormik.values.email}
-                                                onChange={cadastroFormik.handleChange} />
-                                    <FieldError error={cadastroFormik.errors.email} />
-                                </div>
-                                <div>
-                                    <label className='block text-s font-medium leading-6 text-gray-900'>Senha: *</label>
-                                </div>
-                                <div className='mt-2'>
-                                    <InputText style='w-full' 
-                                                id='password' 
-                                                placeholder='Senha'
-                                                type='password'
-                                                value={cadastroFormik.values.password}
-                                                onChange={cadastroFormik.handleChange} />
-                                    <FieldError error={cadastroFormik.errors.password} />
-                                </div>
-                                <div>
-                                    <label className='block text-s font-medium leading-6 text-gray-900'>Confirme a senha: *</label>
-                                </div>
-                                <div className='mt-2'>
-                                    <InputText style='w-full' 
-                                                id='passwordMatch' 
-                                                placeholder='Confirme a senha'
-                                                type='password'
-                                                value={cadastroFormik.values.passwordMatch}
-                                                onChange={cadastroFormik.handleChange} />
-                                    <FieldError error={cadastroFormik.errors.passwordMatch} />
-                                </div>
-                                <Button type='submit' 
-                                                style='bg-indigo-500 hover:bg-indigo-700' 
-                                                label='Salvar' />
+                                                onChange={cadastroFormik.handleChange}
+                                            />
+                                        </div>
+                                        <FieldError error={cadastroFormik.errors.name} />
+                                    </div>
 
-                                <Button type='button' 
-                                        onClick={event => setNewUserState(false)} 
-                                        style='ml-2 bg-red-600 hover:bg-red-500' 
-                                        label='Cancelar'/>
-                            </form>
-                        </RenderIf>
+                                    <div>
+                                        <label className="block text-sm font-medium text-gray-700">Email</label>
+                                        <div className="mt-1 relative rounded-md shadow-sm">
+                                            <div className="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none">
+                                                <FaEnvelope className="text-gray-400" />
+                                            </div>
+                                            <InputText
+                                                style="w-full pl-10 pr-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+                                                id="email"
+                                                placeholder="Email"
+                                                value={cadastroFormik.values.email}
+                                                onChange={cadastroFormik.handleChange}
+                                            />
+                                        </div>
+                                        <FieldError error={cadastroFormik.errors.email} />
+                                    </div>
+
+                                    <div>
+                                        <label className="block text-sm font-medium text-gray-700">Senha</label>
+                                        <div className="mt-1 relative rounded-md shadow-sm">
+                                            <div className="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none">
+                                                <FaLock className="text-gray-400" />
+                                            </div>
+                                            <InputText
+                                                style="w-full pl-10 pr-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+                                                id="password"
+                                                placeholder="Senha"
+                                                type="password"
+                                                value={cadastroFormik.values.password}
+                                                onChange={cadastroFormik.handleChange}
+                                            />
+                                        </div>
+                                        <FieldError error={cadastroFormik.errors.password} />
+                                    </div>
+
+                                    <div>
+                                        <label className="block text-sm font-medium text-gray-700">Confirme a senha</label>
+                                        <div className="mt-1 relative rounded-md shadow-sm">
+                                            <div className="absolute inset-y-0 left-0 pl-3 flex items-center pointer-events-none">
+                                                <FaLock className="text-gray-400" />
+                                            </div>
+                                            <InputText
+                                                style="w-full pl-10 pr-3 py-2 border border-gray-300 rounded-md shadow-sm placeholder-gray-400 focus:outline-none focus:ring-indigo-500 focus:border-indigo-500 sm:text-sm"
+                                                id="passwordMatch"
+                                                placeholder="Confirme a senha"
+                                                type="password"
+                                                value={cadastroFormik.values.passwordMatch}
+                                                onChange={cadastroFormik.handleChange}
+                                            />
+                                        </div>
+                                        <FieldError error={cadastroFormik.errors.passwordMatch} />
+                                    </div>
+
+                                    <Button type="submit" style="w-full bg-indigo-500 hover:bg-indigo-700 text-white py-2 px-4 rounded-md" label="Salvar" />
+                                    <Button type="button" onClick={() => setNewUserState(false)} style="w-full bg-red-600 hover:bg-red-500 text-white py-2 px-4 rounded-md mt-4" label="Cancelar" />
+                                </form>
+                            </RenderIf>
+                        </div>
                     </div>
                 </div>
-            </Template>
         </div>
-    )
+    );
 }
